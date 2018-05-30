db49c2f808832965551ee91bf8c8218359555af7
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-05-30 10:19:41 -0700

    Add updateItem method. Broke will not update anything if it is not in the correct format.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ function addItem (path, item) {
 
   const text = `${count}\t${item.name}\t${item.quantity}\n`
   fs.writeFileSync(path, text, { flag: 'a' })
+  //{ flag: 'a' } in the above line this is an optional parameter which is an object, with three potential properties.  Flag is one of those properties, and the 'a' means - Open file for appending and the file is created if it does not exist.
+
+
 }
 
 function getItem (path, idx) {
@@ -23,7 +26,26 @@ function getItem (path, idx) {
 }
 
 function updateItem (path, idx, item) {
-  
+  //create current variable that will reference the item that's getting updated
+  const current = fs.readFileSync(path, 'utf-8').split('\n');
+  let itemToUpdate = current[idx-1];
+
+
+  //then we need to modify the item name if necessary, and change the item quantity to the new amount using
+  if (!itemToUpdate) {
+    return null;
+  }
+  const arrOfObjProperties = itemToUpdate.split('\t');
+  //properties should look like [count, itemName, quantity]
+  arrOfObjProperties[1] = '\t' + item.name + '\t';
+  arrOfObjProperties[2] = item.quantity;
+  const updatedText = arrOfObjProperties.join('');
+  current[idx-1] = updatedText;
+  const newShoppingList = current.join('\n');
+  console.log(newShoppingList);
+
+  //then write the updated object using writeFileSync
+  fs.writeFileSync(path, newShoppingList);
 }
 
 module.exports = {
